1ae1ec0bb91a4030b23c8d4f4e8b915d9526a910
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2021-05-05 19:42:35 +0700

    Init component SimonTheGame

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SimonTheGame />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import SimonTheGame from './components/SimonTheGame.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    SimonTheGame,
   },
 };
 </script>
